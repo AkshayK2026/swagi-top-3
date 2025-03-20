28c713c8938718439d3993241fc8622e2ac2f87b
AkshayK2026 <akshaykadam.7095@gmail.com>
2025-03-20 21:57:12 +0530

    repo for swagi top 3 sale product

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this project help to find the top 3 items on the swagi',
+    author='Akshay-kadam',
+    license='',
+)
